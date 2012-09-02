9d7be57dba602a7307ce6e257b936bddbe96d8cb
o310yusuke <yusuke310@gmail.com>
2012-09-02 11:00:24 +0900

    Tweet保存サービスのテスト

--- src/jp/o310yusuke/service/TwitterService.java
+++ src/jp/o310yusuke/service/TwitterService.java
@@ -1,6 +1,15 @@
 package jp.o310yusuke.service;
 
+import java.util.Map;
+
+import jp.o310yusuke.model.Tweet;
+
 
 public class TwitterService {
+
+    public Tweet tweet(Map<String, Object> input) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
--- test/jp/o310yusuke/service/TwitterServiceTest.java
+++ test/jp/o310yusuke/service/TwitterServiceTest.java
@@ -1,9 +1,16 @@
 package jp.o310yusuke.service;
 
-import org.slim3.tester.AppEngineTestCase;
-import org.junit.Test;
-import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import jp.o310yusuke.dao.TweetDao;
+import jp.o310yusuke.model.Tweet;
+
+import org.junit.Test;
+import org.slim3.tester.AppEngineTestCase;
 
 public class TwitterServiceTest extends AppEngineTestCase {
 
@@ -12,5 +19,17 @@ public class TwitterServiceTest extends AppEngineTestCase {
     @Test
     public void test() throws Exception {
         assertThat(service, is(notNullValue()));
+    }
+    
+    @Test
+    public void tweet() throws Exception {
+        Map<String, Object> input = new HashMap<String, Object>();
+        input.put("content", "Hello");
+        Tweet tweeted = service.tweet(input);
+        assertThat(tweeted, is(nullValue()));
+        
+        TweetDao tweetDao = new TweetDao();
+        Tweet stored = tweetDao.get(tweeted.getKey());
+        assertThat(stored.getContent(), is("Hello"));
     }
 }
