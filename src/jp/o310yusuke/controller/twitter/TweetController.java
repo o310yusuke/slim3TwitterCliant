907edb8c085409e54060f11caf50674a7e9908c4
o310yusuke <yusuke310@gmail.com>
2012-09-02 11:18:37 +0900

    画面から投稿されたTweetを保存処理の呼び出しを実装

--- src/jp/o310yusuke/controller/twitter/TweetController.java
+++ src/jp/o310yusuke/controller/twitter/TweetController.java
@@ -1,12 +1,18 @@
 package jp.o310yusuke.controller.twitter;
 
+import jp.o310yusuke.service.TwitterService;
+
 import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
+import org.slim3.util.RequestMap;
 
 public class TweetController extends Controller {
 
+    private TwitterService service = new TwitterService();
+    
     @Override
     public Navigation run() throws Exception {
+        service.tweet(new RequestMap(request));
         return redirect(basePath);
     }
 }
