dd7850e04393219a203260c45a10a85c20bac793
o310yusuke <yusuke310@gmail.com>
2012-09-02 11:15:18 +0900

    Tweetを画面入力から取得し、保存するテスト

--- test/jp/o310yusuke/controller/twitter/TweetControllerTest.java
+++ test/jp/o310yusuke/controller/twitter/TweetControllerTest.java
@@ -1,18 +1,30 @@
 package jp.o310yusuke.controller.twitter;
 
-import org.slim3.tester.ControllerTestCase;
-import org.junit.Test;
-import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import jp.o310yusuke.model.Tweet;
+
+import org.junit.Test;
+import org.slim3.datastore.Datastore;
+import org.slim3.tester.ControllerTestCase;
 
 public class TweetControllerTest extends ControllerTestCase {
 
     @Test
     public void run() throws Exception {
+        tester.param("content", "Hello");
+        
         tester.start("/twitter/tweet");
         TweetController controller = tester.getController();
         assertThat(controller, is(notNullValue()));
         assertThat(tester.isRedirect(), is(true));
         assertThat(tester.getDestinationPath(), is("/twitter/"));
+
+        Tweet stored = Datastore.query(Tweet.class).asSingle();
+        assertThat(stored, is(notNullValue()));
+        assertThat(stored.getContent(), is("Hello"));
+        
+        
+        
     }
 }
